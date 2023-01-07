ffd67e180d7a87af85b3fed2ba1b7a03e80285dc
Parinazsiami <Prinzsiami@gmail.com>
2023-01-07 16:08:15 +0330

    Test the api call

--- app/src/main/java/com/example/melobit/MainActivity.java
+++ app/src/main/java/com/example/melobit/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.melobit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.melobit.models.SongsListResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_view);
+        RequestManager manager = new RequestManager(this);
+        SongsListResponseListener listener = new SongsListResponseListener() {
+            @Override
+            public void didFetch(SongsListResponse response, String status) {
+                textView.setText(response.getResults().get(0).getTitle());
+            }
+
+            @Override
+            public void didError(String status) {
+                textView.setText(status);
+            }
+        };
+        manager.getNewSongs(listener);
     }
 }
